312c3eaba7d3707f1d561ad819609c53a911bf98
bigbang7 <bigbang7.2011@gmail.com>
2019-05-15 00:24:59 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+let walls = [];
+let ray;
+let particle;
+
+function setup(){
+    let canvas = createCanvas(600,600);
+    canvas.parent("canvas");
+    for(let i = 0; i < 5; i++){
+        let x1 = random(width);
+        let x2 = random(width);
+        let y1 = random(height);
+        let y2 = random(height);
+        walls[i] = new Boundary(x1,y1,x2, y2);
+    }
+    
+    particle = new Particle();
+
+}
+
+function draw(){
+    background(20);
+    for(let wall of walls){
+        wall.show();
+        
+        
+    }
+    particle.update(mouseX,mouseY);
+    particle.show();
+    particle.look(walls);
+    
+    
+    // ray.show()
+    // ray.lookAt(mouseX,mouseY);
+    // let point = ray.cast(wall);
+    // if(point){
+    //     fill(255);
+    //     ellipse(point.x, point.y, 8, 8);
+    // }
+}
