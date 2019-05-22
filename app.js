6dc9d76487426a0d7d1c8dfdad9409d0bd194949
bigbang7 <bigbang7.2011@gmail.com>
2019-05-22 17:17:54 -0300

    Added Outer Boundaries and increased the number of rays simulated

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ function setup(){
         let y2 = random(height);
         walls[i] = new Boundary(x1,y1,x2, y2);
     }
+    walls.push(new Boundary(0, 0, 0, height));
+    walls.push(new Boundary(0, 0, width, 0));
+    walls.push(new Boundary(width, height, 0, height));
+    walls.push(new Boundary(width, height, width, 0));
     
     particle = new Particle();
 
@@ -21,8 +25,6 @@ function draw(){
     background(20);
     for(let wall of walls){
         wall.show();
-        
-        
     }
     particle.update(mouseX,mouseY);
     particle.show();
